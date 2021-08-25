48d10283209b3d8710d6c5d772e9d24d5d2278cf
Claes 'Letharion' Gyllensvärd <letharion@gmail.com>
2021-08-25 17:44:41 +0200

    Various cleanup after ainu7 fixed the logic to calculate dust correctly.
    Update object.js from Trimps to include WoA fix.
    Remove lots of old and un-needed code.
    Load item configuration from file instead of hard-coding.

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-Autobattle simulator
+# Autobattle simulator
 
-This copies the object.js file from the Trimps source and exposes it's content as a module.
+This copies the object.js file from the Trimps source and exposes its content as a module.
 
 Then it iterates over the fight logic trying to simulate how much dust would be earned.
 
-Currently the dust calculation is buggy and yields incorrect values.
+Put the items you want to use into items.json in a format like this:
+
+    [
+        { "n": "Rusty_Dagger", "l": 18 },
+        { "n": "Lifegiving_Gem", "l": 16 },
+        { "n": "Shock_and_Awl", "l": 13 },
+    ]
--- index.js
+++ index.js
@@ -16,13 +16,8 @@ const html = `<html><head></head><body>
 const dom = new JSDOM(html);
 const readline = require('readline');
 let AB;
-let run;
-let iterations;
 let startTime;
 let formatter;
-let items_to_use;
-
-console.log("Starting");
 
 const start = () => {
     global.document = dom.window.document;
@@ -41,23 +36,6 @@ const start = () => {
     global.swapClass = () => {};
     global.giveSingleAchieve = () => {};
     global.usingRealTimeOffline = false;
-    global.sendEvent = event => {
-        if ("Trimp died" === event || "Enemy died" === event) {
-            if ("Trimp died" === event) {
-                deathCount++;
-                // If we can't win, go back to farming.
-                if (deathCount > 10) {
-                    farming = true;
-                    AB.levelDown();
-                }
-            }
-            else {
-                deathCount = 0;
-            }
-            //doAllUpgrade();
-            //       console.log(event);
-        }
-    }
     global.getRandomIntSeeded = (seed, minIncl, maxExcl) => {
         var toReturn = Math.floor(seededRandom(seed) * (maxExcl - minIncl)) + minIncl;
         return (toReturn === maxExcl) ? minIncl : toReturn;
@@ -69,86 +47,52 @@ const start = () => {
 
     formatter = new Intl.NumberFormat('en-US', { minimumFractionDigits: 3 });
 
-    doAllUpgrade = () => {
-        items_to_use.forEach(x => AB.upgrade(x));
-    }
-
-    //console.log(document.getElementById('autoBattleTrimpHealthBar'));
-    //console.log(document.body.parentElement.innerHTML);
     AB = require('./object.js');
 
-    let deathCount = 0;
-    iterations = 100;
+    let iterations = 100;
     let farming = false;
     let battleTime = 0;
 
     AB.speed = 100;
-    AB.update();
-    AB.enemy.level = 29;
-    AB.oneTimers.Master_of_Arms.owned = true;
 
-    items_to_use = [
-        //{ n: 'Rusty_Dagger', l: 4 },
-        { n: 'Battery_Stick', l: 11 },
-        { n: 'Raincoat', l: 10 },
-        //{ n: 'Bad_Medkit', l: 3 },
-        //{ n: 'Chemistry_Set', l: 10 },
-        { n: 'Lifegiving_Gem', l: 10 },
-        //    { n: 'Spiked_Gloves', l: 4},
-        { n: 'Shock_and_Awl', l: 7 },
-        { n: 'Lich_Wraps', l: 6 },
-        //    { n: 'Aegis', l: 2},
-        //    { n: 'Sword_and_Board', l: 3 },
-        { n: 'Bloodstained_Gloves', l: 6 },
-        //{ n: 'Wired_Wristguards', l: 6 },
-        //{ n: 'Eelimp_in_a_Bottle', l: 4 },
-        //{ n: 'Sacrificial_Shank', l: 5 },
-        { n: 'Big_Cleaver', l: 3 },
-        //{ n: 'Plague_Bringer', l: 2 },
-        //{ n: 'Very_Large_Slime', l: 2 },
-        { n: 'Monkimp_Paw', l: 2 },
-    ];
-    AB.bonuses.Extra_Limbs.level = items_to_use.length - 5;
+    AB.enemyLevel = 45;
+    AB.maxEnemyLevel = 59;
+    AB.oneTimers.Master_of_Arms.owned = true;
+    AB.oneTimers.Dusty_Tome.owned = true;
+    AB.oneTimers.Whirlwind_of_Arms.owned = true;
+
+    // Sword and Pants start out equipped by default, so we need to unequip them.
+    AB.equip("Sword");
+    AB.equip("Pants");
+
+    let items_to_use = require('./items.json');
+
+    AB.bonuses.Extra_Limbs.level = items_to_use.length - 4;
+    console.log(items_to_use);
     items_to_use.forEach(x => {
         AB.items[x.n].level = x.l;
         AB.equip(x.n);
     });
 
-    run = 0;
+    AB.update();
+
+    let run = 0;
     startTime = Date.now();
-    simulate();
+    while (run++ < iterations) {
+      AB.update();
+    }
+    wrapup();
 }
 
-const simulate = () => {
-    AB.update();
-    if (run++ < iterations) {
-        setTimeout(simulate, 300);
-    }
-    else {
-        wrapup();
-    }
-}
 const wrapup = () => {
     const endTime = Date.now();
     const time = endTime - startTime;
     console.log("Took " + time + " ms");
+    console.log(AB.lootAvg.counter + " ms processed.");
+    console.log(AB.sessionEnemiesKilled + " killed, ", AB.sessionTrimpsKilled + " died.");
 
-    base_dust = AB.getDustPs() * 20000;
-    console.log(AB.lootAvg.accumulator);
+    base_dust = AB.getDustPs();
     console.log("Base value: " + formatter.format(base_dust) + " dust per second");
 }
 
-/*items_to_use.forEach(x => {
-    upgradeCost = AB.upgradeCost(x.n);
-    AB.items[x.n].level += 1;
-    run = 0;
-
-    simulate();
-
-    x.newDust = AB.getDustPs();
-    console.log("With 1 more level in " + x.n + ": " + formatter.format(x.newDust) + " at a cost of " + upgradeCost);
-    console.log(formatter.format(upgradeCost / (x.newDust - base_dust)) + " dust cost per % dust gain increase");
-    AB.items[x.n].level -= 1;
-});*/
-
-setTimeout(start, 7);
+start();
